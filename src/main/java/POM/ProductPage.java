7bfc6d4b7834bbf3fb2340cb665d0525fc6cc9c4
Bhargav <Bhargav@HP>
2025-09-23 14:50:50 +0530

    Commiting Product Page by sam

---
+++ src/main/java/POM/ProductPage.java
@@ -0,0 +1,13 @@
+package POM;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductPage {
+	@FindBy(xpath="//input[@alt='Create Product..']")
+	private WebElement createProductImgBtn;
+	
+	@FindBy(className="crmbutton small create")
+	private WebElement searchnow;
+
+}
