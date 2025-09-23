015c0d937cae92bbb13c5c4ecb62e0b6d73df807
Bhargav <Bhargav@HP>
2025-09-23 14:50:50 +0530

    Commiting Product Page

---
+++ src/main/java/POM/ProductPage.java
@@ -0,0 +1,10 @@
+package POM;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductPage {
+	@FindBy(xpath="//input[@alt='Create Product..']")
+	private WebElement createProductImgBtn;
+
+}
